12f5bb01ee2387be2155051f9b4b8e29db6cd44b
merijnads <116170132+merijnads@users.noreply.github.com>
2023-06-30 09:04:56 +0000

    Update course-settings.js

--- course-settings.js
+++ course-settings.js
@@ -1,22 +1,22 @@
 const courseSettings = {
   language: "en",
-  name: "Python Programming MOOC 2023",
-  siteUrl: "https://programming-23.mooc.fi",
+  name: "Programmeren 1",
+  siteUrl: "https://merijnads.github.io",
   githubUrl: "https://github.com/rage/programming-23",
   subtitle: "Python Programming MOOC",
-  slug: "programming-23",
+  slug: "hr-programmeren-1",
   organizationName: "MOOC",
-  tmcCourse: "programming-23",
+  tmcCourse: "hr-programmeren-1",
   quizzesId: "5ffc21a8-5f0d-4c11-871d-ba5fa17b2bc0",
-  tmcOrganization: "mooc",
+  tmcOrganization: "hr",
   bannerPath: "banner.svg",
   showExerciseDescriptionWhenNotLoggedIn: true,
   courseVariants: [
-     {
-       tmcOrganization: "hr",
-       tmcCourse: "1231",
+     //{
+    //   tmcOrganization: "hr",
+     //  tmcCourse: "1231",
        // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-     },
+     //},
   ],
 }
 
