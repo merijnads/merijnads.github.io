a397e86d7754977fe21aed249e70a4d113a712ae
merijnads <116170132+merijnads@users.noreply.github.com>
2023-06-30 08:47:18 +0000

    Update course-settings.js

--- course-settings.js
+++ course-settings.js
@@ -12,11 +12,11 @@ const courseSettings = {
   bannerPath: "banner.svg",
   showExerciseDescriptionWhenNotLoggedIn: true,
   courseVariants: [
-    // {
-    //   tmcOrganization: "hbc",
-    //   tmcCourse: "python-TVT21A",
-    //   // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    // },
+     {
+       tmcOrganization: "hr",
+       tmcCourse: "1231",
+       // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
+     },
   ],
 }
 
